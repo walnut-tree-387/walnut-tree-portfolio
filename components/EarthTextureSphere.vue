4963b8ef40e8592b0b86fe1715bd1d2c977dc3c5
ataurAGI <ataur.rahaman@anwargroup.com>
2025-06-26 22:24:53 +0600

    refactored to dynamic star color

--- components/EarthTextureSphere.vue
+++ components/EarthTextureSphere.vue
@@ -7,12 +7,13 @@ import { onMounted, onBeforeUnmount, ref } from 'vue'
 import * as THREE from 'three'
 import { useEarthTextureSphere } from '../composables/useEarthTextureSphere'
 import { StarService } from '../composables/services/StarService'
+
+const colorMode = useColorMode()
 const containerRef = ref<HTMLElement | null>(null)
 
 let renderer: THREE.WebGLRenderer
 let scene: THREE.Scene
 let cleanup: () => void
-
 onMounted(() => {
   if (!containerRef.value) return
 
@@ -25,6 +26,16 @@ onMounted(() => {
   starService.addStars()
   const setup = useEarthTextureSphere(containerRef.value, scene, renderer)
   cleanup = setup.cleanup
+
+
+  // Changing star color for visibility depending on the theme
+  watch(() => colorMode.value, (newMode) => {
+  if (newMode === 'dark') {
+    starService.updateStarsColor(0xffffff)
+  } else {
+    starService.updateStarsColor(0x333366)
+  }
+}, { immediate: true })
 })
 
 onBeforeUnmount(() => {
