22f01fb39a6841bb9e68ea34fccadfb470c4fe0e
walnut-tree-387 <rahamanmohammadataur@gmail.com>
2025-12-12 23:41:32 +0600

    experience slide fix

--- components/portfolio/Experience.vue
+++ components/portfolio/Experience.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="text-scale flex w-screen h-100vh items-center justify-center">
-    <div class="pl-4">
-        <div class="flex flex-col items-center text-center pb-2">
+  <div class="text-scale flex w-screen h-100vh items-center justify-center border-b border-dashed border-gray-800">
+    <div class="p-2">
+        <div class="flex flex-col items-center text-center p-2">
             <span class="text-lg font-bold">{{ details.companyName }}</span>
             <span class="text-md font-semibold">{{ details.period }}</span>
         </div>
-        <div class="flex flex-wrap gap-2 text-center mt-4">
+        <div class="flex flex-wrap gap-2 text-center mt-2">
             <span
                 v-for="skill in details.skills"
                 :key="skill"
